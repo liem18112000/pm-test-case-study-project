557cee773c8a3c47b06e96bb059eaf48000316f5
tuan431 <66856978+tuan431@users.noreply.github.com>
2021-07-04 20:40:42 +0700

    minor change

--- src/main/java/org/springframework/samples/petclinic/api/PetEndPoint.java
+++ src/main/java/org/springframework/samples/petclinic/api/PetEndPoint.java
@@ -14,28 +14,23 @@ import java.time.LocalDate;
 public class PetEndPoint {
 
 	private final PetRepository petRepository;
+
 	private final OwnerRepository ownerRepository;
 
 	@GetMapping("api/pet/pet-type")
-	public ResponseEntity<?> getPetType(){
-		return ResponseEntity.ok(
-			this.petRepository.findPetTypes()
-		);
+	public ResponseEntity<?> getPetType() {
+		return ResponseEntity.ok(this.petRepository.findPetTypes());
 	}
 
 	@GetMapping("api/pet/{id}")
-	public ResponseEntity<?> getByID(@PathVariable(name="id") int id) {
+	public ResponseEntity<?> getByID(@PathVariable(name = "id") int id) {
 		final Pet target = this.petRepository.findById(id);
-		if(target == null) {
-			return new ResponseEntity<>(
-				"Pet with ID '" + Integer.toString(id) + "' is not found",
-				HttpStatus.NOT_FOUND
-			);
+		if (target == null) {
+			return new ResponseEntity<>("Pet with ID '" + Integer.toString(id) + "' is not found",
+					HttpStatus.NOT_FOUND);
 		}
 
-		return ResponseEntity.ok(
-			target
-		);
+		return ResponseEntity.ok(target);
 	}
 
 	@PostMapping("api/pet")
@@ -45,46 +40,55 @@ public class PetEndPoint {
 		newPet.setBirthDate(request.getBirth_date());
 
 		final PetType petType = this.petRepository.findPetTypeByID(request.getType_id());
-		if(petType == null){
+		if (petType == null) {
 			return new ResponseEntity<>(
-				"Pet type with id '" + Integer.toString(request.getType_id()) + "' is not found",
-				HttpStatus.NOT_FOUND
-			);
-		}else{
+					"Pet type with id '" + Integer.toString(request.getType_id()) + "' is not found",
+					HttpStatus.NOT_FOUND);
+		}
+		else {
 			newPet.setType(petType);
 		}
 
-//		final Owner owner = this.ownerRepository.findById(request.getOwner_id());
-//		if(owner == null) {
-//			return new ResponseEntity<>(
-//				"Owner with id '" + Integer.toString(request.getOwner_id()) + "' is not found",
-//				HttpStatus.NOT_FOUND
-//			);
-//		}else{
-//			newPet.setOwner()
-//		}
+		// final Owner owner = this.ownerRepository.findById(request.getOwner_id());
+		// if(owner == null) {
+		// return new ResponseEntity<>(
+		// "Owner with id '" + Integer.toString(request.getOwner_id()) + "' is not found",
+		// HttpStatus.NOT_FOUND
+		// );
+		// }else{
+		// newPet.setOwner()
+		// }
 
 		this.petRepository.save(newPet);
-		return ResponseEntity.ok(
-			"Create pet successfully"
-		);
+		return ResponseEntity.ok("Create pet successfully");
 	}
 
 	@AllArgsConstructor
 	@Getter
 	static private class CreatePetRequest {
+
 		private final String name;
+
 		private final LocalDate birth_date;
+
 		private final int type_id;
+
 		private final int owner_id;
+
 	}
 
 	@AllArgsConstructor
 	@Getter
 	static private class UpdatePetRequest {
+
 		private String name = null;
+
 		private LocalDate birth_date = null;
+
 		private int type_id = -1;
+
 		private int owner_id = -1;
+
 	}
+
 }
